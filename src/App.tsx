352f6a9259010df83cce06da2470637d968a40bc
lkrre <rrelak@gmail.com>
2025-04-04 20:29:42 -0600

    added home content v1 and some styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { Palette } from './components/Palette'
@@ -10,7 +8,6 @@ import { Projects } from './components/Sections/Projects'
 import { Contact } from './components/Sections/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [currentSection, setCurrentSection] = useState("home");
 
   const renderSection = () => {
@@ -26,32 +23,22 @@ function App() {
   return (
     <>
       <Navbar onSectionChange={(id) => setCurrentSection(id)} />
-      {/* <PixelTransition key={currentSection}> */}
-      <div className='w-full'>
-        {renderSection()}
+      <div id="Content" className='mx-4 md:mx-8 xl:mx-16'>
+        {/* <PixelTransition key={currentSection}> */}
+        <div>
+          {renderSection()}
+        </div>
+        {/* </PixelTransition> */}
+        {/* <Palette/>
+        <div className='flex'>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div> */}
       </div>
-      {/* </PixelTransition> */}
-      <Palette/>
-      <div className='flex'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-pine-green'>Vite + React</h1>
-      <div className="card text-moss-green">
-        <button className='' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className='text-raspberry'>
-          Edit <span className='text-dodger-blue'>src/App.tsx</span> and save to test HMR
-        </p>
-      </div>
-      <p className="text-sunglow">
-        Click on the Vite and <span className='text-light-sky-blue'>React</span> logos to learn more
-      </p>
     </>
   )
 }
--- src/components/Sections/Home.tsx
+++ src/components/Sections/Home.tsx
@@ -1,5 +1,21 @@
+import Linkedin from "../Icons/Linkedin";
+import GitHub from "../Icons/GitHub";
+import CloudDownload from "../Icons/CloudDownload";
+import { useTranslation } from "react-i18next";
+
 export const Home = () => {
+    const { t } = useTranslation();
     return <>
-        <h1>Home</h1>
+        <h1 className="text-3xl my-4">Laura <span className="text-violet">Karina</span> Rámirez</h1>
+        <span className="text-cornflower my-2">{t("software engineer")}</span>
+        <div className="flex justify-center gap-2 my-12">
+            <a href="https://www.linkedin.com/in/rrelak/" target="blank">
+                <Linkedin className="bg-true-blue h-6 w-6 rounded"/>
+            </a>
+            <a href="https://github.com/lkrre" target="blank">
+                <GitHub className="h-6 w-6"/>
+            </a>
+            <CloudDownload className="h-6 w-6"/>
+        </div>
     </>
 }
