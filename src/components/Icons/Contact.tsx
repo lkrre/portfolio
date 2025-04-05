d190ff8d31501294d99fd1a6231d78d5b401e9ac
lkrre <rrelak@gmail.com>
2025-04-04 20:28:51 -0600

    added icons

--- src/components/Icons/Contact.tsx
+++ src/components/Icons/Contact.tsx
@@ -9,8 +9,8 @@ const ContactIcon = ({ className = "w-6 h-6" }: { className?: string }) => (
       xmlns="http://www.w3.org/2000/svg"
     >
       <path 
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M2 3H0V21H2H22H24V3H22H2ZM22 5V19H2V5H22ZM10 7H6V11H10V7ZM4 13H12V17H4V13ZM20 7H14V9H20V7ZM14 11H20V13H14V11ZM20 15H14V17H20V15Z"
       />
     </svg>
