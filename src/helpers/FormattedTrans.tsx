a694f0f19eaf6e8ad3e078e85ab2d5f3aaa7432d
lkrre <rrelak@gmail.com>
2025-04-06 16:44:23 -0600

    added format tranlation to add style tags on my json

---
+++ src/helpers/FormattedTrans.tsx
@@ -0,0 +1,39 @@
+import { Trans, TransProps } from 'react-i18next';
+
+const FormattedTrans = (props: TransProps) => {
+  return (
+    <div>
+        <Trans
+        {...props}
+        components={{
+            italic: <span className="italic" />,
+            gray: <span className="text-gray" />,
+            silver: <span className="text-silver" />,
+            winter: <span className="text-winter" />,
+            pictonBlue: <span className="text-picton-blue" />,
+            carolina: <span className="text-carolina" />,
+            cornflower: <span className="text-cornflower" />,
+            trueBlue: <span className="text-true-blue" />,
+            navyBlue: <span className="text-navy-blue" />,
+            pineGreen: <span className="text-pine-green" />,
+            mayGreen: <span className="text-may-green" />,
+            russianGreen: <span className="text-russian-green" />,
+            eucalyptus: <span className="text-eucalyptus" />,
+            mossGreen: <span className="text-moss-green" />,
+            cAndC: <span className="text-c-and-c" />,
+            burlywood: <span className="text-burlywood" />,
+            sheenGold: <span className="text-sheen-gold" />,
+            antiqueBrass: <span className="text-antique-brass" />,
+            watermelon: <span className="text-watermelon" />,
+            tartOrange: <span className="text-tart-orange" />,
+            lilac: <span className="text-lilac" />,
+            violet: <span className="text-violet" />,
+            blueGray: <span className="text-blue-gray" />,
+            ...(props.components || {})
+        }}
+        />
+    </div>
+  );
+};
+
+export default FormattedTrans;
