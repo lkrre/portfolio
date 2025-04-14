faf17706f2439c96fd863053341994b8afeffbe3
lkrre <rrelak@gmail.com>
2025-04-14 00:31:25 -0600

    arrow icons

---
+++ src/components/Icons/RightArrow.tsx
@@ -0,0 +1,16 @@
+const RightArrow = ({ className = "w-6 h-6" }: { className?: string }) => (
+    <svg
+      className={className}
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="currentColor"
+      stroke="currentColor"
+      shapeRendering="crispEdges"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 0h1M0 1h3M0 2h5M0 3h7M0 4h9M0 5h11M0 6h13M0 7h15M0 8h13M0 9h11M0 10h9M0 11h7M0 12h5M0 13h3M0 14h1"/>
+    </svg>
+  );
+  
+  export default RightArrow;
