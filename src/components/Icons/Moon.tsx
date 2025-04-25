056d24eb183bfd98570998264fe0046b4cbc323d
lkrre <rrelak@gmail.com>
2025-04-25 13:04:03 -0600

    atribute name corrections

--- src/components/Icons/Moon.tsx
+++ src/components/Icons/Moon.tsx
@@ -8,7 +8,7 @@ const Moon = ({ className = "w-6 h-6" }: { className?: string }) => (
       stroke="1"
       xmlns="http://www.w3.org/2000/svg"
     >
-    <g clip-path="url(#clip0_604_3537)">
+    <g clipPath="url(#clip0_604_3537)">
       <path
         fillRule="evenodd"
         clipRule="evenodd"
